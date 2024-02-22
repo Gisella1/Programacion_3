795e2b0b15df9866e433aa4cdcc0bb9dfc5b6918
Gisella Barraza <barrazagisella14@gmail.com>
2024-02-22 11:41:53 -0500

    primer compromiso

---
+++ Main.py
@@ -0,0 +1,4 @@
+from Programacio import Estudiante
+
+p = Estudiante ("Gisella","Barraza","gisella@gmail.com","320526896")
+print(p.Gestinfo())
---
+++ Programacio.py
@@ -0,0 +1,10 @@
+class Estudiante:
+    def __init__(self, nombre, apellido, correo, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.telefono = telefono
+
+    def Gestinfo(self):
+        print(self.nombre)
+
