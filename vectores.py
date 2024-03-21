21dc18ea3105340e9ad9284219948c240672c3e2
Gisella1 <160900003+Gisella1@users.noreply.github.com>
2024-03-21 11:26:26 -0500

    Add files via upload

---
+++ vectores.py
@@ -0,0 +1,33 @@
+#1
+lista_vacia = []
+#2
+Lista_elementos = [1, 2, 3, 4, 5]
+#3
+print (len(Lista_elementos))
+#4
+print (Lista_elementos[0],Lista_elementos[2],Lista_elementos[4])
+#5
+Tipos_datos_mezclados = ['Gisella', 23, 1.55, 'en relacion', 'la maria']
+#6
+it_companies = ['facebook', 'google', 'microsoft', 'apple', 'IBM', 'oracle', 'amazon']
+#7
+it_companies.append('instagram')
+print (it_companies)
+#8
+verificar = 'Apple' in it_companies
+print (verificar) 
+#9
+it_companies.sort()
+print(it_companies)
+#10
+it_companies.reverse()
+print(it_companies)
+#11
+del it_companies[0]
+print(it_companies)
+#12
+it_companies.clear()
+print(it_companies)
+
+
+
